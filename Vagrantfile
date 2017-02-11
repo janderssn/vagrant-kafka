7b3338e6d7fb8437a34c0b12308037e08f7e1c7a
Joel Andersson <joel.andersson@pierce-ecom.com>
2017-02-11 19:05:43 +0100

    systemd + portforward

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,15 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.hostname = "kafka-1"
   config.vm.provision :shell, path: "install_kafka.sh"
-  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "private_network", ip: "10.2.0.10"
+  config.vm.network "forwarded_port", guest: 2181, host: 2181
+  config.vm.network "forwarded_port", guest: 9092, host: 9092
 
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "2048"
+    vb.customize ["modifyvm", :id, "--cpuexecutioncap", "50"]
+    vb.customize ["modifyvm", :id, "--nictype1", "virtio"]
+    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
   end
 end
--- install_kafka.sh
+++ install_kafka.sh
@@ -1,11 +1,23 @@
 sudo apt-get update
 sudo apt-get install openjdk-7-jdk -y
+sudo apt-get install systemd -y
+
+cd /opt
+
 echo "Downloading Kafka, please wait..."
-wget -q http://apache.mirrors.hoobly.com/kafka/0.8.2.1/kafka_2.10-0.8.2.1.tgz
+wget -q http://apache.mirrors.hoobly.com/kafka/0.10.1.1/kafka_2.11-0.10.1.1.tgz
 echo "Download complete, finishing install"
-tar -zxvf kafka_2.10-0.8.2.1.tgz
-cd kafka_2.10-0.8.2.1/
-echo "host.name=192.168.33.10" >> config/server.properties
-bin/zookeeper-server-start.sh config/zookeeper.properties &
-bin/kafka-server-start.sh config/server.properties &
+tar -zxvf kafka_2.11-0.10.1.1.tgz
+ln -s kafka_2.11-0.10.1.1 kafka
+cd kafka
+#echo "host.name=10.2.0.10" >> config/server.properties
+echo "advertised.listeners=PLAINTEXT://127.0.0.1:9092" >> config/server.properties
+cp /vagrant/zookeeper.service /etc/systemd/system
+cp /vagrant/kafka.service /etc/systemd/system
+chmod +x /etc/systemd/system/zookeeper.service
+chmod +x /etc/systemd/system/kafka.service
+systemctl enable zookeeper
+systemctl enable kafka
+systemctl start zookeeper
+systemctl start kafka
 echo "Installation complete!"
